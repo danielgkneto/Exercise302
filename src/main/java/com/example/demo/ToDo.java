83c60a04b58e245a5e334e406b9a2e933c9bc75a
danielgkneto <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-20 12:04:18 -0400

    initial commit

---
+++ src/main/java/com/example/demo/ToDo.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 1, max = 30)
+    private String name;
+
+    @NotNull
+    @Size(min = 1, max = 10)
+    private String dueDate;
+
+    @NotNull
+    @Size(min = 1, max = 10)
+    private String priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
